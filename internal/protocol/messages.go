37dfcf1456fd10e78be63499840931b1c9744896
Lúcás Meier <cronokirby@gmail.com>
2019-03-17 15:15:43 +0100

    Add private definitions for all the message types

--- internal/protocol/messages.go
+++ internal/protocol/messages.go
@@ -1,10 +1,24 @@
 package protocol
 
-// Ping represents a Ping message, that has to be sent from time to time, in
-// order to keep a connection between 2 peers alive
-type Ping struct{}
+import "net"
 
-// JoinRequest represents a request from one peer to join the swarm
+// ping represents a Ping message, that has to be sent from time to time, in
+// order to keep a connection between 2 peers alive
+type ping struct{}
+
+// joinRequest represents a request from one peer to join the swarm
 // chat the other peer is in. The peer receiving this message should
 // respond with its own JoinResponse
-type JoinRequest struct{}
+type joinRequest struct{}
+
+// joinResponse represents the response from a peer after a joinRequest
+type joinResponse struct {
+	// A list of peers we can connect to, and that may try and connect
+	// with us
+	peers []net.Addr
+}
+
+// message represents a message sent to a swarm by a peer
+type message struct {
+	content string
+}
