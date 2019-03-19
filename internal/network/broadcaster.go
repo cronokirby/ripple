ee19e1edfbcb1fba13fdfebfa19cf6e306178c39
Lúcás Meier <cronokirby@gmail.com>
2019-03-19 20:19:20 +0100

    Add broadcaster

---
+++ internal/network/broadcaster.go
@@ -0,0 +1,78 @@
+package network
+
+import (
+	"io"
+	"net"
+	"sync"
+
+	"github.com/cronokirby/ripple/internal/protocol"
+)
+
+// peerList is a list of peers that can be updated concurrently.
+// the zero value of peerList is valid
+type peerList struct {
+	peers []net.Addr
+	lock  sync.Mutex
+}
+
+// addPeers safely adds a list of peers to a peerList
+// this function can be used safely concurrently, but will block
+func (peers *peerList) addPeers(newPeers ...net.Addr) {
+	peers.lock.Lock()
+	defer peers.lock.Unlock()
+	peers.peers = append(peers.peers, newPeers...)
+}
+
+// This function is safe so long as the slice isn't mutated
+func (peers *peerList) getPeers() []net.Addr {
+	return peers.peers
+}
+
+func sendMessage(w io.Writer, msg protocol.Message) error {
+	data := msg.MessageBytes()
+	for len(data) > 0 {
+		written, err := w.Write(data)
+		if err != nil {
+			return err
+		}
+		data = data[written:]
+	}
+	return nil
+}
+
+// Broadcaster represents an object we can send new messages to, and
+// have it broadcast to other peers
+type broadcaster struct {
+	conns      []net.Conn
+	newConns   chan net.Conn
+	newContent chan string
+}
+
+func makeBroadcaster() *broadcaster {
+	return &broadcaster{
+		newConns:   make(chan net.Conn),
+		newContent: make(chan string),
+	}
+}
+
+func (bc *broadcaster) loop() {
+	for {
+		select {
+		case content := <-bc.newContent:
+			for _, conn := range bc.conns {
+				// we don't care about errors
+				sendMessage(conn, protocol.NewMessage{Content: content})
+			}
+		case newConn := <-bc.newConns:
+			bc.conns = append(bc.conns, newConn)
+		}
+	}
+}
+
+func (bc *broadcaster) sendConn(conn net.Conn) {
+	bc.newConns <- conn
+}
+
+func (bc *broadcaster) sendContent(content string) {
+	bc.newContent <- content
+}
--- internal/network/joiner.go
+++ internal/network/joiner.go
@@ -3,53 +3,20 @@ package network
 import (
 	"errors"
 	"fmt"
-	"io"
 	"net"
-	"sync"
 
 	"github.com/cronokirby/ripple/internal/protocol"
 )
 
-// peerList is a list of peers that can be updated concurrently.
-// the zero value of peerList is valid
-type peerList struct {
-	peers []net.Addr
-	lock  sync.Mutex
-}
-
-// addPeers safely adds a list of peers to a peerList
-// this function can be used safely concurrently, but will block
-func (peers *peerList) addPeers(newPeers ...net.Addr) {
-	peers.lock.Lock()
-	defer peers.lock.Unlock()
-	peers.peers = append(peers.peers, newPeers...)
-}
-
-// This function is safe so long as the slice isn't mutated
-func (peers *peerList) getPeers() []net.Addr {
-	return peers.peers
-}
-
-func sendMessage(w io.Writer, msg protocol.Message) error {
-	data := msg.MessageBytes()
-	for len(data) > 0 {
-		written, err := w.Write(data)
-		if err != nil {
-			return err
-		}
-		data = data[written:]
-	}
-	return nil
-}
-
 // Joiner represents a client that tries and join a new swarm
 type Joiner struct {
-	peers *peerList
+	peers       *peerList
+	broadcaster *broadcaster
 }
 
 // NewJoiner creates a valid joiner, since the zero value isn't
 func NewJoiner() Joiner {
-	return Joiner{peers: &peerList{}}
+	return Joiner{peers: &peerList{}, broadcaster: makeBroadcaster()}
 }
 
 // HandlePing exits the connection, because we only expect JoinResponse
@@ -74,12 +41,14 @@ func (j *Joiner) HandleNewMessage(msg protocol.NewMessage) error {
 	return errors.New("Unexpected NewMessage when joining swarm")
 }
 
-// Run blocks the current thread to run a joiner given a certain address,
+// Start blocks the current thread to run a joiner given a certain address,
 // which it will use to enter the swarm
-// This function will exit if it encounters an error
+// This function will exit if it encounters an error before entering the swarm.
+// After entering the swarm, it will become self sufficient, and restart
+// the different parts it manages if necessary
 //
 // myAddr is the address to listen to after first connecting to remoteAddr
-func (j *Joiner) Run(myAddr string, remoteAddr net.Addr) error {
+func (j *Joiner) Start(myAddr string, remoteAddr net.Addr) error {
 	conn, err := net.Dial(remoteAddr.Network(), remoteAddr.String())
 	if err != nil {
 		return err
@@ -100,31 +69,44 @@ func (j *Joiner) Run(myAddr string, remoteAddr net.Addr) error {
 		conn.Close()
 		return err
 	}
-	// Connect to every peer we've been given
-	newConns := make(chan net.Conn)
-	newConns <- conn
-	firstClient := &normalClient{}
-	go firstClient.justLoop(conn)
-	for _, addr := range j.peers.getPeers() {
-		client := &normalClient{}
-		go client.connectAndLoop(addr, newConns)
-	}
-	// We add the first peer now, not wanting to loop over it
-	j.peers.addPeers(conn.RemoteAddr())
-	l, err := net.Listen("tcp", myAddr)
-	if err != nil {
-		return err
-	}
-	defer l.Close()
-	for {
-		conn, err := l.Accept()
-		if err != nil {
-			return err
+	go func() {
+		// Connect to every peer we've been given
+		j.broadcaster.sendConn(conn)
+		firstClient := &normalClient{}
+		go firstClient.justLoop(conn)
+		for _, addr := range j.peers.getPeers() {
+			client := &normalClient{}
+			go client.connectAndLoop(addr, j.broadcaster)
 		}
-		peers := j.peers
-		client := &acceptingClient{newConns, conn, peers}
-		go client.accept()
-	}
+		// We add the first peer now, not wanting to loop over it
+		j.peers.addPeers(conn.RemoteAddr())
+		l, err := net.Listen("tcp", myAddr)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		defer l.Close()
+		for {
+			conn, err := l.Accept()
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			peers := j.peers
+			client := &acceptingClient{j.broadcaster, conn, peers}
+			go client.accept()
+		}
+
+	}()
+	return nil
+}
+
+// SendContent sends a new piece of a content the swarm we're connected to
+//
+// This function will block briefly, but not until the message is completely
+// transmitted.
+func (j *Joiner) SendContent(content string) {
+	j.broadcaster.sendContent(content)
 }
 
 // acceptingClient tries and connect with a newly accepted peer
@@ -132,9 +114,9 @@ type acceptingClient struct {
 	// newConns is a channel we can use to broadcast the new connection
 	// if it's client that behaves correctly, and we enter a normal
 	// relationship in it
-	newConns chan net.Conn
-	conn     net.Conn
-	peers    *peerList
+	broadcaster *broadcaster
+	conn        net.Conn
+	peers       *peerList
 }
 
 func (client *acceptingClient) HandlePing() error {
@@ -147,7 +129,7 @@ func (client *acceptingClient) HandleJoinRequest() error {
 	if err != nil {
 		return err
 	}
-	client.newConns <- client.conn
+	client.broadcaster.sendConn(client.conn)
 	client.peers.addPeers(client.conn.RemoteAddr())
 	newClient := &normalClient{}
 	return newClient.innerLoop(client.conn)
@@ -196,13 +178,13 @@ func (client *normalClient) HandleNewMessage(msg protocol.NewMessage) error {
 
 // connectAndLoop starts a normal client with a new address to connect to,
 // and returns an error whenever something fatal occurrs
-func (client *normalClient) connectAndLoop(addr net.Addr, newConns chan net.Conn) error {
+func (client *normalClient) connectAndLoop(addr net.Addr, bc *broadcaster) error {
 	conn, err := net.Dial(addr.Network(), addr.String())
 	if err != nil {
 		return err
 	}
 	defer conn.Close()
-	newConns <- conn
+	bc.sendConn(conn)
 	err = client.innerLoop(conn)
 	fmt.Println(err)
 	return err
